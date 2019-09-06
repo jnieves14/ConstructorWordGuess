e1a924512236288a929b3d10fa8eefaa3692173e
Jannah June Nieves <jannahjune@gmail.com>
2019-09-06 16:22:42 -0700

    if letter matches return true

--- letter.js
+++ letter.js
@@ -1,17 +1,18 @@
 function Letter(letter) {
-    this.letter = letter;
-    this.guessed = false;
+    this.letter = letter,
+    this.guessed = false,
     this.toString = function() {
-        if (this.guessed === true) {
+        if (this.guessed) {
             return this.letter.toUpperCase();
         } else {
             return "_";
             } 
-        }
+        },
 
     this.checkLetter = function(input) {
         if (input.toLowerCase() === this.letter.toLowerCase()) {
             this.guessed = true;
+            return true;
         } else {
             return false;
         }
